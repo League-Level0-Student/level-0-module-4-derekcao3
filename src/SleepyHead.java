b6c148f171eaf867b76b16ef9e910aba6dc62a95
Cao.Derek <Cao.Derek@SEMS-259-14.SEMS259.LAB>
2018-04-25 14:58:23 -0700

    String Method 2, Get up Sleepyhead

--- src/SleepyHead.java
+++ src/SleepyHead.java
@@ -1,28 +1,37 @@
+
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
 
 import javax.swing.JOptionPane;
 
-
 public class SleepyHead {
 
+	public static void main(String[] args) {
 
-    public static void main(String[] args) {
-        
-        boolean isWeekday, isVacation;
-        
-        /*
-         * Ask the user for these values using
-         * JOptionPane.showConfirmDialog(null, "is it a weekday?", "",
-         * JOptionPane.YES_NO_OPTION);
-         */
-    
+		int isWeekday, isVacation;
+		int yes=0,no =1;
+		/*
+		 * Ask the user for these values using
+		 */
 
+		isWeekday = JOptionPane.showConfirmDialog(null, "is it a weekday?", "", JOptionPane.YES_NO_OPTION);
+		System.out.println(isWeekday);
+		isVacation = JOptionPane.showConfirmDialog(null, "are we on vacation", "", JOptionPane.YES_NO_OPTION);
+		System.out.println(isVacation);
+		if (isWeekday == yes && isVacation == no) {
+			JOptionPane.showMessageDialog(null, "get up lazybones");
+		} else if (isVacation == yes) {
+			JOptionPane.showMessageDialog(null, "sleep in");
+		}
+		 else if (isWeekday == yes) {
+			JOptionPane.showMessageDialog(null, "get up lazybones");
+		 }
 
-        /*
-         * Print “sleep in” if it is a vacation or a weekend. If it’s a weekday,
-         * print “get up lazybones!” If it is a weekday, and we are on vacation,
-         * print “sleep in”.
-         */
-    }
+		/*
+		 * Print "sleep in" if it is a vacation or a weekend. If it's a weekday, print
+		 * "get up lazybones!" If it is a weekday, and we are on vacation, print
+		 * "sleep in".
+		 */
+	
+	}
 }
--- src/StringMethods2.java
+++ src/StringMethods2.java
@@ -14,11 +14,13 @@ public class StringMethods2 {
 		System.out.println(y.toLowerCase());
 		// 4. Print the first 3 char's of your String
 		//    HINT: .substring(start,end)
-		y.substring(beginIndex, endIndex);
+		System.out.println(y.substring(0,3));
 		// 5. Print a single char somewhere in the middle of your String
 		//    REMINDER: char's in string start at index 0
-		
+		System.out.println(y.charAt(3));
 		// 6. BONUS -- print the LAST 3 char's of your string using
 		//        .length() to determine WHERE the last 3 char's are located.
+		System.out.println(y.length());
+		System.out.println(y.substring(2,5));
 	}
 }
