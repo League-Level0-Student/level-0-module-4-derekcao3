2d8e2b6741a57c58a05b6f17dc8c294e6f33692f
Cao.Derek <Cao.Derek@SEMS-259-14.SEMS259.LAB>
2018-05-09 14:52:33 -0700

    For Gauntlet

---
+++ src/InfinityGauntletFortnut.java
@@ -0,0 +1,52 @@
+
+public class InfinityGauntletFortnut {
+	public static void main(String[] args) {
+		/*
+		 * for (int i = 0; i < 101; i++) { System.out.println(i); }
+		 */
+		// for (int j = 100; j >-1 ; j--) {
+		// System.out.println(j);
+		// }
+
+//		for (int i = 2; i < 102; i++) {
+//		if(i%2==0) {
+//			System.out.println(i);
+//		}
+//		for (int i = 0; i < 101; i++) {
+//			if(i%2!=0) {
+//				System.out.println(i);
+//			}
+//		}
+		
+//		for (int i = 0; i < 501; i++) {
+//			if(i%2==0) {
+//				System.out.println(i + " is even");
+//			}
+//			else {
+//				System.out.println(i +" is odd");
+//			}
+//		}
+		
+//		for (int i = 0; i < 778; i++) {
+//			if(i%7==0) {
+//				System.out.println(i);
+//			}
+//		}
+//		int j=0;
+//		for (int i = 2005; i < 2019; i++, j++) {
+//	System.out.println("In " + i + ", I was " + j + " years old");
+//} 
+//		for (int i = 0; i < 3; i++) {
+//			for (int j = 0; j < 3; j++) {
+//				System.out.println(i + " " + j);
+//			}
+//		}
+//		
+		for (int i = 0; i < 7; i++) {
+			System.out.println("* ");
+			
+		}
+	}
+
+}
+		
